ddb9c6a7ccafce7a1cf1afdbba905eb7e24de4d9
FrancisHoste00 <francishoste@gmail.com>
2015-04-22 17:27:17 +0200

    Sql bestand

--- project2.sql
+++ project2.sql
@@ -1,136 +1,256 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Machine: 127.0.0.1
+-- Gegenereerd op: 22 apr 2015 om 17:26
+-- Serverversie: 5.6.21
+-- PHP-versie: 5.6.3
 
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `mydb` ;
-
--- -----------------------------------------------------
--- Table `mydb`.`UserGroup`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`UserGroup` (
-  `idUserGroup` BIGINT NOT NULL,
-  `Group` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`idUserGroup`),
-  UNIQUE INDEX `idUserGroup_UNIQUE` (`idUserGroup` ASC))
-ENGINE = InnoDB;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
--- -----------------------------------------------------
--- Table `mydb`.`Users`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Users` (
-  `idUsers` BIGINT NOT NULL,
-  `userName` VARCHAR(45) NOT NULL,
-  `passWord` VARCHAR(45) NOT NULL,
-  `UserGroup_idUserGroup` INT NOT NULL,
-  PRIMARY KEY (`idUsers`, `UserGroup_idUserGroup`),
-  UNIQUE INDEX `idUsers_UNIQUE` (`idUsers` ASC),
-  INDEX `fk_Users_UserGroup_idx` (`UserGroup_idUserGroup` ASC),
-  CONSTRAINT `fk_Users_UserGroup`
-    FOREIGN KEY (`UserGroup_idUserGroup`)
-    REFERENCES `mydb`.`UserGroup` (`idUserGroup`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
+--
+-- Databank: `project2`
+--
 
--- -----------------------------------------------------
--- Table `mydb`.`Examinees`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Examinees` (
-  `idExaminees` BIGINT NOT NULL,
-  `firstName` VARCHAR(45) NOT NULL,
-  `lastName` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(45) NULL,
-  `dateExam` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `UserGroup_idUserGroup` BIGINT NOT NULL,
-  PRIMARY KEY (`idExaminees`, `UserGroup_idUserGroup`),
-  UNIQUE INDEX `idExaminees_UNIQUE` (`idExaminees` ASC),
-  INDEX `fk_Examinees_UserGroup1_idx` (`UserGroup_idUserGroup` ASC),
-  CONSTRAINT `fk_Examinees_UserGroup1`
-    FOREIGN KEY (`UserGroup_idUserGroup`)
-    REFERENCES `mydb`.`UserGroup` (`idUserGroup`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+-- --------------------------------------------------------
 
+--
+-- Tabelstructuur voor tabel `exam`
+--
 
--- -----------------------------------------------------
--- Table `mydb`.`Exam`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Exam` (
-  `idExam` BIGINT NOT NULL,
-  `date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`idExam`),
-  UNIQUE INDEX `idExam_UNIQUE` (`idExam` ASC))
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `exam` (
+`idExam` bigint(20) NOT NULL,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
 
--- -----------------------------------------------------
--- Table `mydb`.`Questions`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Questions` (
-  `idQuestions` BIGINT NOT NULL,
-  `questionSet` TINYINT NOT NULL,
-  `question` VARCHAR(45) NOT NULL,
-  `correctAnswer` VARCHAR(45) NOT NULL,
-  `alternativeAnswer1` VARCHAR(45) NOT NULL,
-  `alternativeAnswer2` VARCHAR(45) NOT NULL,
-  `Exam_idExam` BIGINT NOT NULL,
-  PRIMARY KEY (`idQuestions`, `Exam_idExam`),
-  UNIQUE INDEX `idQuestionSet_UNIQUE` (`idQuestions` ASC),
-  INDEX `fk_Questions_Exam1_idx` (`Exam_idExam` ASC),
-  CONSTRAINT `fk_Questions_Exam1`
-    FOREIGN KEY (`Exam_idExam`)
-    REFERENCES `mydb`.`Exam` (`idExam`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+--
+-- Tabelstructuur voor tabel `examinees`
+--
 
+CREATE TABLE IF NOT EXISTS `examinees` (
+`idExaminees` bigint(20) NOT NULL,
+  `firstName` varchar(45) NOT NULL,
+  `lastName` varchar(45) NOT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `dateExam` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `UserGroup_idUserGroup` bigint(20) NOT NULL DEFAULT '3'
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
--- -----------------------------------------------------
--- Table `mydb`.`ImageLink`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`ImageLink` (
-  `idImageLink` BIGINT NOT NULL,
-  `Original` VARCHAR(45) NOT NULL,
-  `highRes` VARCHAR(45) NOT NULL,
-  `lowRes` VARCHAR(45) NOT NULL,
-  `Questions_idQuestions` BIGINT NOT NULL,
-  PRIMARY KEY (`idImageLink`, `Questions_idQuestions`),
-  UNIQUE INDEX `idImageLink_UNIQUE` (`idImageLink` ASC),
-  INDEX `fk_ImageLink_Questions1_idx` (`Questions_idQuestions` ASC),
-  CONSTRAINT `fk_ImageLink_Questions1`
-    FOREIGN KEY (`Questions_idQuestions`)
-    REFERENCES `mydb`.`Questions` (`idQuestions`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+--
+-- Gegevens worden geëxporteerd voor tabel `examinees`
+--
 
+INSERT INTO `examinees` (`idExaminees`, `firstName`, `lastName`, `email`, `dateExam`, `UserGroup_idUserGroup`) VALUES
+(1, 'Francis', 'Hoste', 'hallo@kroket.be', '2015-04-01 17:48:39', 3),
+(3, 'Jopie', 'VandeGraskant', 'hallo@kroket.be', '2015-04-01 17:53:57', 3);
 
--- -----------------------------------------------------
--- Table `mydb`.`Score`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Score` (
-  `score` INT NOT NULL,
-  `Exam_idExam` INT NOT NULL,
-  `Examinees_idExaminees` INT NOT NULL,
-  PRIMARY KEY (`Exam_idExam`, `Examinees_idExaminees`),
-  INDEX `fk_Score_Examinees1_idx` (`Examinees_idExaminees` ASC),
-  CONSTRAINT `fk_Score_Exam1`
-    FOREIGN KEY (`Exam_idExam`)
-    REFERENCES `mydb`.`Exam` (`idExam`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Score_Examinees1`
-    FOREIGN KEY (`Examinees_idExaminees`)
-    REFERENCES `mydb`.`Examinees` (`idExaminees`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+-- --------------------------------------------------------
 
+--
+-- Tabelstructuur voor tabel `imagelink`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+CREATE TABLE IF NOT EXISTS `imagelink` (
+`idImageLink` bigint(20) NOT NULL,
+  `Original` varchar(45) NOT NULL,
+  `highRes` varchar(45) NOT NULL,
+  `lowRes` varchar(45) NOT NULL,
+  `Questions_idQuestions` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `questions`
+--
+
+CREATE TABLE IF NOT EXISTS `questions` (
+`idQuestions` bigint(20) NOT NULL,
+  `questionSet` tinyint(4) NOT NULL,
+  `question` varchar(45) NOT NULL,
+  `correctAnswer` varchar(45) NOT NULL,
+  `alternativeAnswer1` varchar(45) NOT NULL,
+  `alternativeAnswer2` varchar(45) NOT NULL,
+  `Exam_idExam` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `score`
+--
+
+CREATE TABLE IF NOT EXISTS `score` (
+  `score` int(11) DEFAULT NULL,
+  `Exam_idExam` bigint(20) NOT NULL,
+  `Examinees_idExaminees` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `usergroup`
+--
+
+CREATE TABLE IF NOT EXISTS `usergroup` (
+`idUserGroup` bigint(20) NOT NULL,
+  `Group` varchar(45) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `usergroup`
+--
+
+INSERT INTO `usergroup` (`idUserGroup`, `Group`) VALUES
+(1, 'Admin'),
+(2, 'Examinator'),
+(3, 'User');
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+`idUsers` bigint(20) NOT NULL,
+  `userName` varchar(45) NOT NULL,
+  `passWord` varchar(45) NOT NULL,
+  `UserGroup_idUserGroup` bigint(20) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `users`
+--
+
+INSERT INTO `users` (`idUsers`, `userName`, `passWord`, `UserGroup_idUserGroup`) VALUES
+(1, 'Francis', '123', 1);
+
+--
+-- Indexen voor geëxporteerde tabellen
+--
+
+--
+-- Indexen voor tabel `exam`
+--
+ALTER TABLE `exam`
+ ADD PRIMARY KEY (`idExam`), ADD UNIQUE KEY `idExam_UNIQUE` (`idExam`);
+
+--
+-- Indexen voor tabel `examinees`
+--
+ALTER TABLE `examinees`
+ ADD PRIMARY KEY (`idExaminees`,`UserGroup_idUserGroup`), ADD UNIQUE KEY `idExaminees_UNIQUE` (`idExaminees`), ADD KEY `fk_Examinees_UserGroup1_idx` (`UserGroup_idUserGroup`);
+
+--
+-- Indexen voor tabel `imagelink`
+--
+ALTER TABLE `imagelink`
+ ADD PRIMARY KEY (`idImageLink`,`Questions_idQuestions`), ADD UNIQUE KEY `idImageLink_UNIQUE` (`idImageLink`), ADD UNIQUE KEY `Questions_idQuestions_UNIQUE` (`Questions_idQuestions`), ADD UNIQUE KEY `Original_UNIQUE` (`Original`), ADD UNIQUE KEY `highRes_UNIQUE` (`highRes`), ADD UNIQUE KEY `lowRes_UNIQUE` (`lowRes`), ADD KEY `fk_ImageLink_Questions1_idx` (`Questions_idQuestions`);
+
+--
+-- Indexen voor tabel `questions`
+--
+ALTER TABLE `questions`
+ ADD PRIMARY KEY (`idQuestions`,`Exam_idExam`), ADD UNIQUE KEY `idQuestionSet_UNIQUE` (`idQuestions`), ADD KEY `fk_Questions_Exam1_idx` (`Exam_idExam`);
+
+--
+-- Indexen voor tabel `score`
+--
+ALTER TABLE `score`
+ ADD PRIMARY KEY (`Examinees_idExaminees`,`Exam_idExam`), ADD UNIQUE KEY `Exam_idExam_UNIQUE` (`Exam_idExam`), ADD UNIQUE KEY `Examinees_idExaminees_UNIQUE` (`Examinees_idExaminees`), ADD KEY `fk_Score_Examinees1_idx` (`Examinees_idExaminees`);
+
+--
+-- Indexen voor tabel `usergroup`
+--
+ALTER TABLE `usergroup`
+ ADD PRIMARY KEY (`idUserGroup`), ADD UNIQUE KEY `idUserGroup_UNIQUE` (`idUserGroup`);
+
+--
+-- Indexen voor tabel `users`
+--
+ALTER TABLE `users`
+ ADD PRIMARY KEY (`idUsers`,`UserGroup_idUserGroup`), ADD UNIQUE KEY `idUsers_UNIQUE` (`idUsers`), ADD KEY `fk_Users_UserGroup_idx` (`UserGroup_idUserGroup`);
+
+--
+-- AUTO_INCREMENT voor geëxporteerde tabellen
+--
+
+--
+-- AUTO_INCREMENT voor een tabel `exam`
+--
+ALTER TABLE `exam`
+MODIFY `idExam` bigint(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT voor een tabel `examinees`
+--
+ALTER TABLE `examinees`
+MODIFY `idExaminees` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT voor een tabel `imagelink`
+--
+ALTER TABLE `imagelink`
+MODIFY `idImageLink` bigint(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT voor een tabel `questions`
+--
+ALTER TABLE `questions`
+MODIFY `idQuestions` bigint(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT voor een tabel `usergroup`
+--
+ALTER TABLE `usergroup`
+MODIFY `idUserGroup` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT voor een tabel `users`
+--
+ALTER TABLE `users`
+MODIFY `idUsers` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- Beperkingen voor geëxporteerde tabellen
+--
+
+--
+-- Beperkingen voor tabel `examinees`
+--
+ALTER TABLE `examinees`
+ADD CONSTRAINT `fk_Examinees_UserGroup1` FOREIGN KEY (`UserGroup_idUserGroup`) REFERENCES `usergroup` (`idUserGroup`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Beperkingen voor tabel `imagelink`
+--
+ALTER TABLE `imagelink`
+ADD CONSTRAINT `fk_ImageLink_Questions1` FOREIGN KEY (`Questions_idQuestions`) REFERENCES `questions` (`idQuestions`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Beperkingen voor tabel `questions`
+--
+ALTER TABLE `questions`
+ADD CONSTRAINT `fk_Questions_Exam1` FOREIGN KEY (`Exam_idExam`) REFERENCES `exam` (`idExam`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Beperkingen voor tabel `score`
+--
+ALTER TABLE `score`
+ADD CONSTRAINT `fk_Score_Exam1` FOREIGN KEY (`Exam_idExam`) REFERENCES `exam` (`idExam`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `fk_Score_Examinees1` FOREIGN KEY (`Examinees_idExaminees`) REFERENCES `examinees` (`idExaminees`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Beperkingen voor tabel `users`
+--
+ALTER TABLE `users`
+ADD CONSTRAINT `fk_Users_UserGroup` FOREIGN KEY (`UserGroup_idUserGroup`) REFERENCES `usergroup` (`idUserGroup`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
