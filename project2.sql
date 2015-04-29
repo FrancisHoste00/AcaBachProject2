6950f665ddd99dcaac2073d36157e958e7ed186d
GVanBuggenhout <gerd.vanbuggenhout@hotmail.com>
2015-04-29 16:47:09 +0200

    Update project2.sql

--- project2.sql
+++ project2.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Machine: 127.0.0.1
--- Gegenereerd op: 22 apr 2015 om 17:58
+-- Gegenereerd op: 22 apr 2015 om 17:26
 -- Serverversie: 5.6.21
 -- PHP-versie: 5.6.3
 
@@ -87,29 +87,6 @@ CREATE TABLE IF NOT EXISTS `questions` (
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `results`
---
-
-CREATE TABLE IF NOT EXISTS `results` (
-`idResults` int(11) NOT NULL,
-  `correct` tinyint(1) NOT NULL,
-  `Exam_idExam` bigint(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Tabelstructuur voor tabel `role`
---
-
-CREATE TABLE IF NOT EXISTS `role` (
-`role_id` bigint(20) NOT NULL,
-  `Group` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
 -- Tabelstructuur voor tabel `score`
 --
 
@@ -146,11 +123,18 @@ INSERT INTO `usergroup` (`idUserGroup`, `Group`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-`usr_id` bigint(20) NOT NULL,
-  `usr_name` varchar(45) NOT NULL,
-  `usr_password` varchar(45) NOT NULL,
-  `usrl_id` bigint(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+`idUsers` bigint(20) NOT NULL,
+  `userName` varchar(45) NOT NULL,
+  `passWord` varchar(45) NOT NULL,
+  `UserGroup_idUserGroup` bigint(20) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `users`
+--
+
+INSERT INTO `users` (`idUsers`, `userName`, `passWord`, `UserGroup_idUserGroup`) VALUES
+(1, 'Francis', '123', 1);
 
 --
 -- Indexen voor geëxporteerde tabellen
@@ -181,18 +165,6 @@ ALTER TABLE `questions`
  ADD PRIMARY KEY (`idQuestions`,`Exam_idExam`), ADD UNIQUE KEY `idQuestionSet_UNIQUE` (`idQuestions`), ADD KEY `fk_Questions_Exam1_idx` (`Exam_idExam`);
 
 --
--- Indexen voor tabel `results`
---
-ALTER TABLE `results`
- ADD PRIMARY KEY (`idResults`,`Exam_idExam`), ADD KEY `fk_Results_Exam1_idx` (`Exam_idExam`);
-
---
--- Indexen voor tabel `role`
---
-ALTER TABLE `role`
- ADD PRIMARY KEY (`role_id`), ADD UNIQUE KEY `idUserGroup_UNIQUE` (`role_id`);
-
---
 -- Indexen voor tabel `score`
 --
 ALTER TABLE `score`
@@ -208,7 +180,7 @@ ALTER TABLE `usergroup`
 -- Indexen voor tabel `users`
 --
 ALTER TABLE `users`
- ADD PRIMARY KEY (`usr_id`,`usrl_id`), ADD UNIQUE KEY `idUsers_UNIQUE` (`usr_id`), ADD KEY `fk_Users_UserGroup_idx` (`usrl_id`);
+ ADD PRIMARY KEY (`idUsers`,`UserGroup_idUserGroup`), ADD UNIQUE KEY `idUsers_UNIQUE` (`idUsers`), ADD KEY `fk_Users_UserGroup_idx` (`UserGroup_idUserGroup`);
 
 --
 -- AUTO_INCREMENT voor geëxporteerde tabellen
@@ -235,16 +207,6 @@ MODIFY `idImageLink` bigint(20) NOT NULL AUTO_INCREMENT;
 ALTER TABLE `questions`
 MODIFY `idQuestions` bigint(20) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT voor een tabel `results`
---
-ALTER TABLE `results`
-MODIFY `idResults` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT voor een tabel `role`
---
-ALTER TABLE `role`
-MODIFY `role_id` bigint(20) NOT NULL AUTO_INCREMENT;
---
 -- AUTO_INCREMENT voor een tabel `usergroup`
 --
 ALTER TABLE `usergroup`
@@ -253,10 +215,16 @@ MODIFY `idUserGroup` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 -- AUTO_INCREMENT voor een tabel `users`
 --
 ALTER TABLE `users`
-MODIFY `usr_id` bigint(20) NOT NULL AUTO_INCREMENT;
+MODIFY `idUsers` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- Beperkingen voor geëxporteerde tabellen
 --
+
+--
+-- Beperkingen voor tabel `examinees`
+--
+ALTER TABLE `examinees`
+ADD CONSTRAINT `fk_Examinees_UserGroup1` FOREIGN KEY (`UserGroup_idUserGroup`) REFERENCES `usergroup` (`idUserGroup`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Beperkingen voor tabel `imagelink`
@@ -271,17 +239,17 @@ ALTER TABLE `questions`
 ADD CONSTRAINT `fk_Questions_Exam1` FOREIGN KEY (`Exam_idExam`) REFERENCES `exam` (`idExam`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Beperkingen voor tabel `results`
---
-ALTER TABLE `results`
-ADD CONSTRAINT `fk_Results_Exam1` FOREIGN KEY (`Exam_idExam`) REFERENCES `exam` (`idExam`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
 -- Beperkingen voor tabel `score`
 --
 ALTER TABLE `score`
 ADD CONSTRAINT `fk_Score_Exam1` FOREIGN KEY (`Exam_idExam`) REFERENCES `exam` (`idExam`) ON DELETE CASCADE ON UPDATE CASCADE,
 ADD CONSTRAINT `fk_Score_Examinees1` FOREIGN KEY (`Examinees_idExaminees`) REFERENCES `examinees` (`idExaminees`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Beperkingen voor tabel `users`
+--
+ALTER TABLE `users`
+ADD CONSTRAINT `fk_Users_UserGroup` FOREIGN KEY (`UserGroup_idUserGroup`) REFERENCES `usergroup` (`idUserGroup`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
